37b96e2921ed9a1c6a9c738bb797382d9c16cdbb
Shakhzod16 <qosimov0805@gmail.com>
2026-01-27 16:11:51 +0500

    lesson

---
+++ Python lesson-1/app.py
@@ -0,0 +1,50 @@
+a = 5
+b = 6
+print(a + b)
+
+# 1️⃣ int — Butun sonlar
+age = 21
+year = 2026
+
+# 2️⃣ float — O‘nli sonlar
+price = 19.99
+height = 1.75
+
+# 3️⃣ complex — Kompleks sonlar
+z = 3 + 4j
+
+# 4️⃣ str — Matn (string)
+name = "Shahzod"
+message = 'Hello Python'
+
+# 5️⃣ bool — Mantiqiy qiymat
+is_active = True
+is_admin = False
+
+# 6️⃣ list — Ro‘yxat (o‘zgaruvchan)
+numbers = [1, 2, 3, 4]
+names = ["Ali", "Vali"]
+
+# 7️⃣ tuple — O‘zgarmas ro‘yxat
+colors = ("red", "green", "blue")
+
+# 8️⃣ set — Takrorlanmas elementlar
+unique_numbers = {1, 2, 3, 3}
+# Natija: {1, 2, 3}
+
+# 9️⃣ dict — Kalit-qiymat juftligi
+user = {
+    "name": "Shahzod",
+    "age": 21
+}
+
+# 🔟 NoneType — Bo‘sh qiymat
+result = None
+
+# 🔍 Ma’lumot turini bilish
+x = 10
+# print(type(x))  # <class 'int'>
+
+# Topshiriq
+# 1 - masala
+
